90eea95d2965bf4f4b47d1faadb4c2cf7af16ae3
dobroillya <dobroillya@gmail.com>
2023-07-18 09:27:38 +0300

    add shoping card test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Card from './Card'
 
 function App() {
 
   return (
     <>
       <h1 className="text-3xl font-bold p-8">
-      Hello world!
+      <Card/>
     </h1>
     </>
   )
