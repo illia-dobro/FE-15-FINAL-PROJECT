6de2867a7d5a6f044fd5f26e994f0988b810795a
Rostyslav Klym <rostik.klym@gmail.com>
2023-07-20 11:41:59 +0300

    added Catalog page basic structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Modal from './modules/common/components/Modal'
+import Catalog from "./pages/catalog";
 
 function App() {
 
   return (
     <>
       <h1 className="text-3xl font-bold p-8">
-      <Modal openModal={true}>
-        test
-       </Modal>
+          <Modal openModal={true}>
+              test
+          </Modal>
+          <Catalog/>
     </h1>
     </>
   )
---
+++ client/src/pages/catalog/Catalog.jsx
@@ -0,0 +1,9 @@
+import styles from "./Catalog.module.scss"
+
+const Catalog = () => {
+    return <div className={styles.main}>
+        Catalog page
+    </div>
+}
+
+export default Catalog
