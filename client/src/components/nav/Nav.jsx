cd745494f84e0fb9ffdf0b1218983c40ba9750be
Diana-Chassot <diana.chassot25@gmail.com>
2023-07-20 11:50:47 +0200

    styles nav-mobile added

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react";
 import { useRef } from "react";
 import Logo from "../logo/Logo";
 
-import { GoPerson,GoSearch } from 'react-icons/go';
-import { LiaShoppingBagSolid } from 'react-icons/Lia';
+import { GoPerson, GoSearch } from "react-icons/go";
+import { LiaShoppingBagSolid } from "react-icons/Lia";
+import { TbMenu } from "react-icons/Tb";
 import "./nav.scss";
 
 function Nav() {
@@ -42,40 +43,62 @@ function Nav() {
   return (
     <>
       {!isMobile ? (
-        <nav className="nav">
-          <div>
-            <Link className="nav__link" to="/catalog">Catalog</Link>
-            <Link className="nav__link" to="/about">About</Link>
-            <Link className="nav__link" to="/delivery">Delivery</Link>
+        <nav className="nav nav-desc">
+          <div className="nav__links">
+            <Link className="nav__link" to="/catalog">
+              Catalog
+            </Link>
+            <Link className="nav__link" to="/about">
+              About
+            </Link>
+            <Link className="nav__link" to="/delivery">
+              Delivery
+            </Link>
           </div>
           <div>
             <Logo />
           </div>
           <div className="nav__right">
-            <Link className="nav__link" to="/contacts">Contacts</Link>
-            
+            <Link className="nav__link" to="/contacts">
+              Contacts
+            </Link>
+
             <div className="nav__icons">
-              <span className="nav__icon" ><GoSearch /></span>
-              <Link className="nav__icon" to="/shop"><LiaShoppingBagSolid/></Link>  
-              <Link className="nav__icon" to="/login"><GoPerson/></Link>
+              <span className="nav__icon">
+                <GoSearch />
+              </span>
+              <Link className="nav__icon" to="/shop">
+                <LiaShoppingBagSolid />
+              </Link>
+              <Link className="nav__icon" to="/login">
+                <GoPerson />
+              </Link>
             </div>
           </div>
         </nav>
       ) : (
-        <nav className="nav-mobile">
+        <nav className="nav nav-mobile">
           <div onClick={() => setOnOpenNav(!onOpenNav)}>
-            <button>Hi</button>
+            <button>
+              <TbMenu />
+            </button>
           </div>
           {onOpenNav && (
-            <div className="nav-mobile__links" ref={navRef}>
-              <Link to="/catalog">Catalog</Link>
-              <Link to="/about">About</Link>
-              <Link to="/delivery">Delivery</Link>
-              <Link to="/contacts">Contacts</Link>
+            <div className="nav__mobile__dropdown" ref={navRef}>
+              <div className="nav-mobile__links">
+                <Link to="/">Home</Link>
+                <Link to="/catalog">Catalog</Link>
+                <Link to="/about">About</Link>
+                <Link to="/delivery">Delivery</Link>
+                <Link to="/contacts">Contacts</Link>
+              </div>
             </div>
           )}
+
           <Logo />
-          <Link to="/shop">Shop-icon</Link>
+          <Link className="nav__icon" to="/shop">
+            <LiaShoppingBagSolid />
+          </Link>
         </nav>
       )}
     </>
