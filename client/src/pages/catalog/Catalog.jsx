104b4b9f0e24525ad54cad65535e22aa071831ef
Rostyslav Klym <rostik.klym@gmail.com>
2023-07-22 15:52:55 +0300

    added basic styles for Catalog page and products list

--- client/src/pages/catalog/Catalog.jsx
+++ client/src/pages/catalog/Catalog.jsx
@@ -1,9 +1,9 @@
-import ProductsList from "./components/productsList";
+import ProductsList from "../../layouts/productsList";
 import styles from "./Catalog.module.scss"
 
 
 const Catalog = () => {
-    return <div className={styles.main}>
+    return <div className={styles.catalog_page}>
         <ProductsList/>
     </div>
 }
