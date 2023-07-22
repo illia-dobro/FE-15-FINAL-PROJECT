6046aca3d1a3c2580107b2ef657a2530ea4fbedc
Rostyslav Klym <rostik.klym@gmail.com>
2023-07-22 17:57:43 +0300

    added Category layout

--- client/src/layouts/category/Category.jsx
+++ client/src/layouts/category/Category.jsx
@@ -1,13 +1,23 @@
 import ProductsList from "../productsList/index.js";
 import { useParams } from "react-router-dom";
+import { categories } from "../../pages/catalog/Catalog.jsx";
 
 const Category = () => {
   const { categoryName } = useParams();
 
+  const category = categories.find(
+    (category) => category.path === categoryName
+  );
+  console.log(category);
+
   return (
     <>
+      <h2 className="text-center text-7xl mx-auto my-4">{category.name}</h2>
 
-        <h2 className="text-center text-7xl pt-4">{categoryName}</h2>
+        <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
+        {" "}
+        <img src={category.img} alt={"banner"} className='w-full h-80'/>
+      </div>
 
       <ProductsList />
     </>
--- client/src/pages/catalog/Catalog.jsx
+++ client/src/pages/catalog/Catalog.jsx
@@ -1,11 +1,56 @@
-import ProductsList from "../../layouts/productsList";
-import styles from "./Catalog.module.scss"
+import { Link, Outlet } from "react-router-dom";
 
+import styles from "./Catalog.module.scss";
+
+export const categories = [
+  {
+    id: "1",
+    path: "care",
+    name: "Care cosmetics",
+    img: "https://e0.pxfuel.com/wallpapers/951/581/desktop-wallpaper-mountains-river-valley-landscape-iceland-dual-wide-background-iceland-dual-monitor.jpg",
+  },
+  {
+    id: "2",
+    path: "decorative",
+    name: "Decorative cosmetics ",
+    img: "https://e0.pxfuel.com/wallpapers/647/161/desktop-wallpaper-aurora-borealis-nature-iceland-dual-wide-widescreen-16-9-widescreen.jpg",
+  },
+  {
+    id: "3",
+    path: "eyebrow",
+    name: "Eyebrow cosmetics",
+    img: "https://e1.pxfuel.com/desktop-wallpaper/46/973/desktop-wallpaper-aldeyjarfoss-waterfall-iceland-winter-ultra-backgrounds-for-u-tv-widescreen-ultrawide-laptop-tablet-smartphone-ultra-wide-winter.jpg",
+  },
+  {
+    id: "4",
+    path: "accessories",
+    name: "Accessories",
+    img: "https://e0.pxfuel.com/wallpapers/776/168/desktop-wallpaper-shore-iceland-%E2%9D%A4-for-ultra-tv-%E2%80%A2-wide.jpg",
+  },
+];
 
 const Catalog = () => {
-    return <div className={styles.catalog_page}>
-        <ProductsList/>
-    </div>
-}
+  return (
+    <div className={styles.catalog_page}>
+      <ul className="mx-auto flex grow justify-evenly text-center text-gray-500">
+        {categories.map((category) => (
+          <li key={category.name}>
+            <Link
+              to={category.path}
+              className="flex justify-center border-b-4 border-transparent hover:text-[#AC8F78] py-4"
+            >
+              {category.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
 
-export default Catalog
+      <Outlet />
+
+      {/*<Unique/>*/}
+      {/*<Footer/>*/}
+    </div>
+  );
+};
+
+export default Catalog;
