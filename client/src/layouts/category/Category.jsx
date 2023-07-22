41c5a1c69fc027478787db417c0317a9e5ed8f7b
Rostyslav Klym <rostik.klym@gmail.com>
2023-07-22 18:45:08 +0300

    added filters templates

--- client/src/layouts/category/Category.jsx
+++ client/src/layouts/category/Category.jsx
@@ -1,6 +1,7 @@
 import ProductsList from "../productsList/index.js";
 import { useParams } from "react-router-dom";
 import { categories } from "../../pages/catalog/Catalog.jsx";
+import Filters from "../../components/filters/index.js";
 
 const Category = () => {
   const { categoryName } = useParams();
@@ -13,12 +14,18 @@ const Category = () => {
     <>
       <h2 className="text-center text-7xl mx-auto my-4">{category.name}</h2>
 
-        <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
         {" "}
-        <img src={category.img} alt={"banner"} className='w-full h-80 object-cover'/>
+        <img
+          src={category.img}
+          alt={"banner"}
+          className="w-full h-80 object-cover"
+        />
       </div>
 
-      <ProductsList category={category.path}/>
+      <Filters>
+        <ProductsList category={category.path} />
+      </Filters>
     </>
   );
 };
