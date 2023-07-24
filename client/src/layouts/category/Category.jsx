8d626c60ff86465fd7e13a5f148251f6bc6396d6
Rostyslav Klym <rostik.klym@gmail.com>
2023-07-24 11:29:59 +0300

    fixed banner styles

--- client/src/layouts/category/Category.jsx
+++ client/src/layouts/category/Category.jsx
@@ -15,11 +15,10 @@ const Category = () => {
       <h2 className="text-center text-7xl mx-auto my-4">{category.name}</h2>
 
       <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
-        {" "}
         <img
-          src={category.img}
-          alt={"banner"}
-          className="w-full h-80 object-cover"
+            src={category.img}
+            alt={"banner"}
+            className="w-full h-80 object-cover overflow-hidden rounded-md"
         />
       </div>
 
