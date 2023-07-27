a4e6e10f65c057dfd2c2d57ab890bf645610058b
Dmytro Lysakov <dimalusakovqwe@gmail.com>
2023-07-27 13:57:05 +0300

    try to fix eslint config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-import "./assets/styles/common.scss"
-import './index.scss'
-import './assets/css/styles.scss'
-
+import './assets/styles/common.scss';
+import './index.scss';
+import './assets/css/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
