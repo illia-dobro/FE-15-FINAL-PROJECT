047d68478b6d059390cda57f57a5d21c94f8895e
Marianna Haleliuka <marianna.galelyuka@gmail.com>
2023-08-07 21:36:43 +0300

    some conflict resolving

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/nav";
 import Home from "./pages";
 import About from "./pages/About";
-import Catalog from "./pages/catalog";
+import Catalog from "./pages/Catalog";
 import Delivery from "./pages/Delivery";
 import Contacts from "./pages/Contacts";
 import Shop from "./pages/Shop";
 import Login from "./pages/Client";
 import PageNotFound from "./pages/PageNotFound";
-import Category from "./layouts/category";
-import Footer from './layouts/footer'
-import Modal from "./components/Modal";
-import ProductDetail from "./pages/ProductDetail";
+// import Modal from './components/Modal'
+
 
 function App() {
   return (
@@ -19,21 +18,15 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route path="/" element={<Home/>} index/>
-          <Route path="/catalog" element={<Catalog/>}>
-            {/*Temporary solution for default route */}
-            <Route index element={<Navigate to="/catalog/care" replace={true}/>}/>
-            <Route path=":categoryName" element={<Category/>}/>
-          </Route>
-          <Route path="/delivery" element={<Delivery/>}/>
+          <Route path="/" element={<Home />} index/>
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/delivery" element={<Delivery />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product/:url" element={<ProductDetail />} />
+          <Route path="/about"  element={<About />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-        <Footer />
       </Router>
     </>
   );
