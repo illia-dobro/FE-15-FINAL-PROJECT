78c8edd1c3a42a6f6ca0ccbe29f41bee08f7926e
Rostyslav Klym <rostik.klym@gmail.com>
2023-08-09 14:59:14 +0300

    added category filter

--- client/src/layouts/category/Category.jsx
+++ client/src/layouts/category/Category.jsx
@@ -9,17 +9,21 @@ import {
 const Category = () => {
   const { categoryName } = useParams();
 
-  const { data: categories } = useCategoriesQuery();
-
+  const { data: categories, isSuccess: isCategoriesSuccess } =
+    useCategoriesQuery();
   const { data: products, isSuccess } = useProductsQuery();
+
+  const category =
+    isCategoriesSuccess &&
+    categories.find((category) => category.name === categoryName);
 
   return (
     <>
-      <h2 className="text-center text-7xl mx-auto my-4">{categoryName}</h2>
+      <h2 className="text-center text-7xl mx-auto my-4">{category.name}</h2>
 
       <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
         <img
-          src={categoryName.img}
+          src={category.imgUrl}
           alt={"banner"}
           className="w-full h-80 object-cover overflow-hidden rounded-md"
         />
