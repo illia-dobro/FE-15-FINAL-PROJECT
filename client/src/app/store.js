8107b71e23485e16adb902b9352dc45851ccea7c
Rostyslav Klym <rostik.klym@gmail.com>
2023-08-09 14:12:53 +0300

    added categories fetch

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import { api } from './services/api';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import productsReducer from "./slices/productsSlice";
+import { api } from "./services/api";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    products: productsReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
