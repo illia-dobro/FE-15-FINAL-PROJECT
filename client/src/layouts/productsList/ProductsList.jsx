42ea9b40e3123ae1ef7475d97987ccc0e450c33c
Rostyslav Klym <rostik.klym@gmail.com>
2023-08-10 12:06:34 +0300

    deleted unused code

--- client/src/layouts/productsList/ProductsList.jsx
+++ client/src/layouts/productsList/ProductsList.jsx
@@ -2,8 +2,6 @@ import ProductCard from "../../components/productCard/index.js";
 import styles from "../../components/productCard/ProductCard.module.scss";
 
 const ProductsList = ({ products }) => {
-  console.log(products);
-
   return (
     <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
@@ -11,7 +9,6 @@ const ProductsList = ({ products }) => {
           <ProductCard
             key={product.itemNo}
             product={product}
-            href={product.href}
             className="group"
           />
         ))}
