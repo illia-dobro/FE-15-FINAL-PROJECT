3e7a177b610092d94f2f100202a4559ac8d64a17
dobroillya <dobroillya@gmail.com>
2023-08-10 08:19:19 +0300

    copy from old signUp to new))

---
+++ client/src/app/services/api.js
@@ -0,0 +1,55 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getLocalStorage } from '../../helpers/localStorage';
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:4000/api',
+    // prepareHeaders: (headers, { getState }) => {
+    //   const token = getState().auth.token;
+    //   if (token) {
+    //     headers.set('authorization', `Bearer ${token}`);
+    //   } else if (getLocalStorage()) {
+    //     headers.set('authorization', `Bearer ${getLocalStorage()}`);
+    //   }
+    //   return headers;
+    // },
+  }),
+  keepUnusedDataFor: 0,
+  endpoints: (builder) => ({
+    getUser: builder.query({
+      query: () => ({
+        url: 'auth/authenticated-user',
+        method: 'GET',
+      }),
+    }),
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: 'auth/sign-in',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+    register: builder.mutation({
+      query(data) {
+        return {
+          url: 'customers',
+          method: 'POST',
+          body: data,
+        };
+      },
+    }),
+    products: builder.query({
+      query: () => ({
+        url: 'products',
+        method: 'GET',
+      }),
+    }),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useRegisterMutation,
+  useGetUserQuery,
+  useProductsQuery,
+} = api;
