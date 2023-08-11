c285b3e173efb1539b68dbb621d622ad49c6ca33
dobroillya <dobroillya@gmail.com>
2023-08-11 08:55:53 +0300

    + redirect auth user to home, styling

--- client/src/layouts/forms/SignIn/SignIn.jsx
+++ client/src/layouts/forms/SignIn/SignIn.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { toast } from 'react-toastify';
-import { useNavigate, Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../../assets/images/logo.svg';
-import { useLoginMutation } from '../../../app/services/api';
+import { useNavigate, Link, Navigate } from 'react-router-dom';
+import { useLoginMutation, useGetUserQuery } from '../../../app/services/api';
 import { saveState } from '../../../helpers/localStorage';
+import { glass } from '../forms.module.scss'
 
 function SignIn() {
   const [email, setEmail] = React.useState('');
@@ -14,6 +14,12 @@ function SignIn() {
 
   const navigate = useNavigate();
 
+  const {isSuccess} = useGetUserQuery();
+
+  if (isSuccess) {
+    return  <Navigate to="/" replace={true} />
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
@@ -22,7 +28,6 @@ function SignIn() {
         loginOrEmail: email,
         password,
       });
-      console.log(data);
       if (data.error) {
         throw data;
       } else {
@@ -31,7 +36,6 @@ function SignIn() {
         navigate('/');
       }
     } catch (err) {
-      console.log(err);
       if (err.error.status === 404 || err.error.status === 400) {
         setError(err.error.data);
       } else {
@@ -43,18 +47,17 @@ function SignIn() {
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Logo fill={'#AC8F78'} className="mb-10 mx-auto" />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-[#555555]">
-          Authorization
+          Sign In
         </h2>
         <p className="mt-2 px-4 pt-15px text-center text-sm text-gray-600">
-          Please enter your username and password from your account to log in,
-          or register
+          Please enter your username and password from your account to sign in,
+          or sign up
         </p>
       </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+      <div className={`${glass} mt-8 sm:mx-auto sm:w-full sm:max-w-md`}>
+        <div className="px-4 py-8 shadow sm:rounded-lg sm:px-10">
           <form className="space-y-6" onSubmit={handleSubmit}>
             <span className="text-red-500 text-sm">
               {error.loginOrEmail}
@@ -109,7 +112,7 @@ function SignIn() {
                 // disabled={isLoading}
                 className="flex w-full justify-center text-[#555555] rounded-md bg-[#EEE4DA] px-3 py-2 text-sm font-semibold shadow-sm hover:bg-[#eddac7] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:bg-[#eddac7] transition"
               >
-                Sign in
+                Sign In
               </button>
             </div>
           </form>
