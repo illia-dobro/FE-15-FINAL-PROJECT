d48ce08fa8ee86bdde5e6e5d37c7526f493e95f1
Dmytro Lysakov <dimalusakovqwe@gmail.com>
2023-08-13 19:02:14 +0300

    fixed style to formik

--- client/src/components/Input/index.jsx
+++ client/src/components/Input/index.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 import { useField } from 'formik';
+import classNames from 'classnames';
 
 const Input = ({ label, ...props }) => {
 	const [field, meta] = useField(props);
 
-	// Визначаємо клас для div в залежності від умови
-	let itemClass = props.itemClass;
-	if (props.index <= 2) {
-		itemClass += ` ${itemClass}--33`;
-	} else if (props.index <= 5) {
-		itemClass += ` ${itemClass}--50`;
-	} else if (props.index <= 7) {
-		itemClass += ` ${itemClass}--25`;
-	}
+	const containerClass = classNames(
+		props.fieldClass,
+		props.itemClass,
+		{
+			[`${props.itemClass}--33`]: props.index <= 2,
+			[`${props.itemClass}--50`]: props.index > 2 && props.index <= 5,
+			[`${props.itemClass}--25`]: props.index > 5 && props.index <= 7,
+		}
+	);
+
+	const inputClass = classNames(props.inputClass, {
+		'_active': field.value !== ''
+	});
 
 	return (
-		<div className={itemClass}>
-			<label
-				htmlFor={props.id}
-				className={props.labelClass}>
+		<div className={containerClass}>
+			<label htmlFor={props.id} className={props.labelClass}>
 				{label}
 			</label>
 			<input
-				className={props.inputClass}
+				className={inputClass}
 				type={props.type}
 				placeholder={props.placeholder}
 				id={props.id}
 				{...field}
 				value={field.value || ''}
-				readOnly={props.name === '_id'} />
-			{meta.touched && meta.error
-				? <div
-					className={props.errorClass}>
-					{meta.error}
-				</div>
-				: null}
+				readOnly={props.name === '_id'}
+			/>
+			{meta.touched && meta.error ? (
+				<div className={props.errorClass}>{meta.error}</div>
+			) : null}
 		</div>
 	);
 };
