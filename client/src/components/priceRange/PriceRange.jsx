aa47c39b74da55c4649293121849e5fae4a36c63
Rostyslav Klym <rostik.klym@gmail.com>
2023-08-15 16:48:00 +0300

    added styles for inputs

--- client/src/components/priceRange/PriceRange.jsx
+++ client/src/components/priceRange/PriceRange.jsx
@@ -7,34 +7,27 @@ const PriceRange = () => {
   const { min, currentMin, currentMax, max } = useSelector(
     (state) => state.filters.priceRange
   );
-
   const dispatch = useDispatch();
 
   const handleRangeChange = (value) => {
     dispatch(setCurrentPriceRange(value));
-    console.log("range changed");
   };
 
   const handleMinChange = (event) => {
     const newMin = +event.target.value;
-    console.log("newMin changed:", [newMin, currentMax]);
-    if (newMin < min || newMin >= currentMax) return; // Змінено умову
-
+    if (newMin < min || newMin >= currentMax) return;
     dispatch(setCurrentPriceRange([newMin, currentMax]));
-    console.log("newMin changed disp");
   };
 
   const handleMaxChange = (event) => {
     const newMax = +event.target.value;
-    console.log("newMax changed:", [currentMin, newMax]);
-    if (newMax > max || newMax <= currentMin) return; // Змінено умову
-
+    if (newMax > max || newMax <= currentMin) return;
     dispatch(setCurrentPriceRange([currentMin, newMax]));
-    console.log("newMax changed disp");
   };
 
   return (
     <div>
+      <p>Price</p>
       <Slider
         range
         allowCross={false}
@@ -46,15 +39,49 @@ const PriceRange = () => {
         }}
       />
 
-      <label>
-        Min Price
-        <input type="number" value={currentMin} onChange={handleMinChange} />
-      </label>
+      <div>
+        <label
+          htmlFor="minPrice"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          from
+        </label>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <span className="text-gray-500 sm:text-sm">$</span>
+          </div>
+          <input
+            type="number"
+            value={currentMin}
+            onChange={handleMinChange}
+            name="minPrice"
+            id="minPrice"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
 
-      <label>
-        Max Price
-        <input type="number" value={currentMax} onChange={handleMaxChange} />
-      </label>
+      <div>
+        <label
+          htmlFor="maxPrice"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          to
+        </label>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <span className="text-gray-500 sm:text-sm">$</span>
+          </div>
+          <input
+            type="number"
+            value={currentMax}
+            onChange={handleMaxChange}
+            name="maxPrice"
+            id="maxPrice"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
     </div>
   );
 };
