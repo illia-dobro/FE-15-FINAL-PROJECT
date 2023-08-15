117dfdf8ad8864706573e9d3ecf15bf0087aef11
Rostyslav Klym <rostik.klym@gmail.com>
2023-08-15 17:45:49 +0300

    updated priceRange styles

--- client/src/components/priceRange/PriceRange.jsx
+++ client/src/components/priceRange/PriceRange.jsx
@@ -39,47 +39,49 @@ const PriceRange = () => {
         }}
       />
 
-      <div>
-        <label
-          htmlFor="minPrice"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          from
-        </label>
-        <div className="relative mt-2 rounded-md shadow-sm">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <span className="text-gray-500 sm:text-sm">$</span>
+      <div className="flex">
+        <div>
+          <label
+            htmlFor="minPrice"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            from
+          </label>
+          <div className="relative mt-2 rounded-md shadow-sm">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="text-gray-500 sm:text-sm">$</span>
+            </div>
+            <input
+              type="number"
+              value={currentMin || ""}
+              onChange={handleMinChange}
+              name="minPrice"
+              id="minPrice"
+              className="block w-1/2 text-right rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
           </div>
-          <input
-            type="number"
-            value={currentMin || ""}
-            onChange={handleMinChange}
-            name="minPrice"
-            id="minPrice"
-            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
         </div>
-      </div>
 
-      <div>
-        <label
-          htmlFor="maxPrice"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          to
-        </label>
-        <div className="relative mt-2 rounded-md shadow-sm">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <span className="text-gray-500 sm:text-sm">$</span>
+        <div>
+          <label
+            htmlFor="maxPrice"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            to
+          </label>
+          <div className="relative mt-2 rounded-md shadow-sm">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="text-gray-500 sm:text-sm">$</span>
+            </div>
+            <input
+              type="number"
+              value={currentMax || ""}
+              onChange={handleMaxChange}
+              name="maxPrice"
+              id="maxPrice"
+              className="block w-1/2 text-right rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
           </div>
-          <input
-            type="number"
-            value={currentMax || ""}
-            onChange={handleMaxChange}
-            name="maxPrice"
-            id="maxPrice"
-            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
         </div>
       </div>
     </div>
