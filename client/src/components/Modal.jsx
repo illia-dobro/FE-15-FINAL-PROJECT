9dcbf8eb1f43427ccbc15d5338e25e7023b85670
dobroillya <dobroillya@gmail.com>
2023-08-16 18:23:14 +0300

    + fix closing modal-2

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,23 +1,23 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Fragment, useRef} from 'react';
+import { Fragment, useRef } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 
-export default function Modal({ children, openModal, setOpen }){
+export default function Modal({ children, openModal, setOpen }) {
 
   const cancelButtonRef = useRef(null);
 
   return (
-    <Transition.Root show={openModal} as={Fragment}>
+    <Transition.Root show={openModal} as={Fragment} appear={true}>
       <Dialog
-        title='modal'
+
         as="div"
         className="relative z-10"
         initialFocus={cancelButtonRef}
         onClose={setOpen}
       >
         <Transition.Child
-
           as={Fragment}
           enter="ease-out duration-300"
           enterFrom="opacity-0"
@@ -41,7 +41,12 @@ export default function Modal({ children, openModal, setOpen }){
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-[#f5ece3] px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                {children}{' '}
+                <Dialog.Title
+                  as="div"
+                >
+                  {children}
+                </Dialog.Title>
+
                 <XMarkIcon
                   onClick={() => setOpen(false)}
                   className="h-6 w-6 text-zinc-600 cursor-pointer absolute top-1 right-1"
@@ -59,5 +64,5 @@ export default function Modal({ children, openModal, setOpen }){
 Modal.propTypes = {
   children: PropTypes.node,
   openModal: PropTypes.bool,
-  setOpen: PropTypes.func
+  setOpen: PropTypes.func,
 };
