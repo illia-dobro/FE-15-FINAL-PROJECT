19dbf95296cd62fa6edde32e6b46a464d2c4db92
Rostyslav Klym <rostik.klym@gmail.com>
2023-08-19 22:15:22 +0300

    added search functionality

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -3,17 +3,28 @@ import { useDispatch, useSelector } from "react-redux";
 import { setSearchQuery } from "../../app/slices/searchSlice.js";
 import ProductsList from "../../layouts/productsList/ProductsList.jsx";
 import { useEffect } from "react";
+import ProductCard from "../productCard/ProductCard.jsx";
+import HeartsLoader from "../heartsLoader/heartsLoader.jsx";
+import { NavLink } from "react-router-dom";
 
 const Search = () => {
   const dispatch = useDispatch();
   const searchQuery = useSelector((state) => state.search.searchQuery);
 
-  const [search, { data: searchResults, isSuccess: isSearchSuccess }] =
-    useSearchProductMutation();
+  const [
+    search,
+    {
+      data: searchResults,
+      isLoading: isSearchLoading,
+      isSuccess: isSearchSuccess,
+    },
+  ] = useSearchProductMutation();
 
   const handleSearchInput = async (e) => {
     dispatch(setSearchQuery(e.target.value));
   };
+
+  console.log(isSearchSuccess, searchResults);
 
   useEffect(() => {
     if (searchQuery.query) {
@@ -22,7 +33,7 @@ const Search = () => {
   }, [search, searchQuery]);
 
   return (
-    <div className="absolute left-1/2 transform -translate-x-1/2 w-64 h-12 bg-white shadow-xl">
+    <div className="absolute left-1/2 transform -translate-x-1/2 w-64 bg-white shadow-xl">
       <div className="container mx-auto pb-4 text-black">
         <input
           id="searchfield"
@@ -30,10 +41,24 @@ const Search = () => {
           placeholder="Search..."
           autoFocus="autofocus"
           className="w-full text-grey-800 transition focus:outline-none focus:border-transparent p-2 appearance-none leading-normal text-xl lg:text-2xl"
-          onBlur={(e) => handleSearchInput(e)}
+          onChange={(e) => handleSearchInput(e)}
         />
       </div>
-     {isSearchSuccess && <ProductsList products={searchResults}  />}
+      <div className="flex flex-col align-middle mb-4">
+        {isSearchLoading && (
+          <HeartsLoader wrapperClass="flex justify-center align-middle" />
+        )}
+        {searchResults?.length ? (
+          <>
+            <ProductsList products={searchResults.slice(0, 4)} />
+            <NavLink to="/catalog" className="text-center">
+              {"Open full catalog"}
+            </NavLink>
+          </>
+        ) : (
+          <span className="text-center">{"No results"}</span>
+        )}
+      </div>
     </div>
   );
 };
