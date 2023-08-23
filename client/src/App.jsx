4e6bbe8ebe3018ac3a38593d11eb45e464e7e930
Rostyslav Klym <rostik.klym@gmail.com>
2023-08-23 20:36:34 +0300

    finished store saving

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,6 +60,9 @@ function App() {
             serverCart?.products,
             stateCart
           );
+          console.log("server", serverCart?.products);
+          console.log("state", stateCart);
+          console.log("merged", mergedCart);
           const updatedCart = await updateCart({ products: mergedCart });
           console.log(updatedCart.data.products);
           dispatch(initializeCart(updatedCart.data.products));
--- client/src/helpers/combineUniqueProducts.js
+++ client/src/helpers/combineUniqueProducts.js
@@ -4,8 +4,20 @@ export const combineUniqueProducts = (...arrays) => {
   );
   const mergedArray = filteredArrays.flat();
 
-  return mergedArray.filter(
-    (item, index, self) =>
-      self.findIndex((otherItem) => otherItem._id === item._id) === index
-  );
+  return mergedArray.reduce((result, currentItem) => {
+    const existingItemIndex = result.findIndex(
+      (item) => item.product && item.product._id === currentItem.product._id
+    );
+
+    if (existingItemIndex !== -1) {
+      result[existingItemIndex] = {
+        ...result[existingItemIndex],
+        cartQuantity: result[existingItemIndex].cartQuantity + 1
+      };
+    } else {
+      result.push(currentItem);
+    }
+
+    return result;
+  }, []);
 };
