4bd0fcc540144fd01b19dd9870a8a64823448276
Marianna Haleliuka <marianna.galelyuka@gmail.com>
2023-08-28 21:47:52 +0300

    products added and styles fixed

--- client/src/components/cosmeticCategory/CosmeticCategory.jsx
+++ client/src/components/cosmeticCategory/CosmeticCategory.jsx
@@ -1,21 +1,34 @@
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useGetFilteredProductsQuery } from '../../app/services/productApi';
+import ProductCard from '../productCard';
 
 import styles from './CosmeticCategory.module.scss';
 
-const CosmeticCategory = ({ image = {}, category = '', to = ''}) => {
+const CosmeticCategory = ({ category = {}}) => {
+    const categoryName = category.name.slice(0,4);
+
+    const { data: productsData, isSuccess: isProductsSuccess } = useGetFilteredProductsQuery(`categories=${category.name}`);
+    const categoryProducts = isProductsSuccess && productsData.products;
+
     return (
-        <NavLink to={to} className={styles.category}>
-            <img src={image.src} alt={image.alt} className={styles.category__image} />
-            <p className={styles.category__name}>{category}</p>
-        </NavLink>
+        <div className={styles.category} >
+            <NavLink to={`/catalog/${category.name}`} className={styles.category__link}>
+                <img src={category.imgUrl} alt={`${categoryName} Cosmetics Product`} className={styles.category__image} />
+                <p className={styles.category__name}>{categoryName}</p>
+            </NavLink>
+
+            <div className={styles.category__products}>
+                {categoryProducts && categoryProducts.slice(0,3).map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
     )
 }
 
 CosmeticCategory.propTypes = {
-    image: PropTypes.object,
-    category: PropTypes.string,
-    to: PropTypes.string
+    category: PropTypes.object,
 }
 
 export default CosmeticCategory;
