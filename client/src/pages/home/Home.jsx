78e53bb8ac496ee9959d58f4128fdaf9df3253b6
Marianna Haleliuka <marianna.galelyuka@gmail.com>
2023-08-29 13:53:43 +0300

    styles fixed

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -176,98 +176,101 @@ function Home() {
 	return (
 		<div className={styles.home}>
 			<HomePageSlider />
-			<div className={styles.home__cosmeticsContainer}>
-				{categories &&
-					categories.slice(0, 3).map((category, index) => (
-						<div
-							className={`${styles.home__careCosmetics} ${index === 1 ? styles.reverseFlexDirection : ''}`}
-							key={category.name}
-						>
-							<CosmeticCategory
-								image={{ src: category.imgUrl, alt: category.name }}
-								category={category.id}
-								to={`/catalog/${category.id}`}
-							/>
-							<div className={styles.home__cosmeticsImages}>
-								{/* Відображення товарів для даної категорії */}
-								{randomProductsByCategory[category.name] &&
-									randomProductsByCategory[category.name].map((product) => (
-										<ProductCard key={product.itemNo} product={product} />
-									))}
+
+			<div className={styles.home__container}>
+				<div className={styles.home__cosmeticsContainer}>
+					{categories &&
+						categories.slice(0, 3).map((category, index) => (
+							<div
+								className={`${styles.home__cosmeticsCategory} ${index === 1 ? styles.reverseFlexDirection : ''}`}
+								key={category.name}
+							>
+								<CosmeticCategory
+									image={{ src: category.imgUrl, alt: category.name }}
+									category={category.id}
+									to={`/catalog/${category.id}`}
+								/>
+								<div className={styles.home__cosmeticsProducts}>
+									{/* Відображення товарів для даної категорії */}
+									{randomProductsByCategory[category.name] &&
+										randomProductsByCategory[category.name].map((product) => (
+											<ProductCard key={product.itemNo} product={product} />
+										))}
+								</div>
 							</div>
-						</div>
-					))}
-			</div>
-			<div className={styles.home__uniqueContainer}>
-				<Unique
-					imageUrl={uniqueMainImgUrl}
-					isRepresentative={true}
-					flexDirection="row-reverse"
-					focus="Each Boltaeva brand product is truly unique - everything is thought out to the smallest detail"
-					content="The brand seeks to build respect among the audience for its products, so that the presence of the company's products is a sign of prestige, since the focus of the business is focused on exclusive sales, but not on the mass market"
-				/>
-			</div>
+						))}
+				</div>
+				<div className={styles.home__uniqueContainer}>
+					<Unique
+						imageUrl={uniqueMainImgUrl}
+						isRepresentative={true}
+						flexDirection="row-reverse"
+						focus="Each Boltaeva brand product is truly unique - everything is thought out to the smallest detail"
+						content="The brand seeks to build respect among the audience for its products, so that the presence of the company's products is a sign of prestige, since the focus of the business is focused on exclusive sales, but not on the mass market"
+					/>
+				</div>
 
-			<div className={styles.home__socialMediaContainer}>
-				<SocialMedia text="Boltaeva - this is a real society of girls who show love to themselves" />
+				<div className={styles.home__socialMediaContainer}>
+					<SocialMedia text="Boltaeva - this is a real society of girls who show love to themselves" />
 
-				<div className={styles.home__socialMediaBtn}>
-					<SocialMediaBtn
-						icons={[
+					<div className={styles.home__socialMediaBtn}>
+						<SocialMediaBtn
+							icons={[
+								{
+									src: InstagramUrl,
+									alt: "Instagram icon",
+									to: "https://www.instagram.com/beauty.shop.ek/",
+								},
+							]}
+						/>
+					</div>
+				</div>
+
+				<div className={styles.home__mediaImages_visible}>
+					<SocialMediaImages
+						socialMediaImages={[
 							{
-								src: InstagramUrl,
-								alt: "Instagram icon",
-								to: "https://www.instagram.com/beauty.shop.ek/",
+								src: SocialImgUrl1,
+								alt: "Social Media Image",
+							},
+							{
+								src: SocialImgUrl2,
+								alt: "Social Media Image",
+							},
+							{
+								src: SocialImgUrl1,
+								alt: "Social Media Image",
+							},
+							{
+								src: SocialImgUrl2,
+								alt: "Social Media Image",
 							},
 						]}
 					/>
 				</div>
-			</div>
 
-			<div className={styles.home__mediaImages_visible}>
-				<SocialMediaImages
-					socialMediaImages={[
-						{
-							src: SocialImgUrl1,
-							alt: "Social Media Image",
-						},
-						{
-							src: SocialImgUrl2,
-							alt: "Social Media Image",
-						},
-						{
-							src: SocialImgUrl1,
-							alt: "Social Media Image",
-						},
-						{
-							src: SocialImgUrl2,
-							alt: "Social Media Image",
-						},
-					]}
-				/>
-			</div>
-
-			<div className={styles.home__mediaImages_hidden}>
-				<SocialMediaImages
-					socialMediaImages={[
-						{
-							src: SocialImgUrl3,
-							alt: "Social Media Image",
-						},
-						{
-							src: SocialImgUrl4,
-							alt: "Social Media Image",
-						},
-						{
-							src: SocialImgUrl3,
-							alt: "Social Media Image",
-						},
-						{
-							src: SocialImgUrl4,
-							alt: "Social Media Image",
-						},
-					]}
-				/>
+				<div className={styles.home__mediaImages_hidden}>
+					<SocialMediaImages
+						socialMediaImages={[
+							{
+								src: SocialImgUrl3,
+								alt: "Social Media Image",
+							},
+							{
+								src: SocialImgUrl4,
+								alt: "Social Media Image",
+							},
+							{
+								src: SocialImgUrl3,
+								alt: "Social Media Image",
+							},
+							{
+								src: SocialImgUrl4,
+								alt: "Social Media Image",
+							},
+						]}
+					/>
+				</div>
 			</div>
 		</div>
 	);
