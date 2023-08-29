a0af774c8b429f55a65e4af3378eda04d563084d
Rostyslav Klym <rostik.klym@gmail.com>
2023-08-29 16:25:39 +0300

    temporary solution for products list style

--- client/src/layouts/productsList/ProductsList.jsx
+++ client/src/layouts/productsList/ProductsList.jsx
@@ -11,7 +11,9 @@ const ProductsList = ({
   return (
     <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
       <div
-        className={`grid grid-cols-${cols} gap-x-6 gap-y-10 sm:grid-cols-${colsSm} lg:grid-cols-${colsLg} xl:grid-cols-${colsXl} xl:gap-x-8`}
+        // className={`grid grid-cols-${cols} gap-x-6 gap-y-10 sm:grid-cols-${colsSm} lg:grid-cols-${colsLg} xl:grid-cols-${colsXl} xl:gap-x-8`}
+
+        className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
       >
         {products.map((product) => (
           <ProductCard
