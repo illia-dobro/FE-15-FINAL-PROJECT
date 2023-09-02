6beca85842bbd742a147a04322de6428d859318f
dobroillya <dobroillya@gmail.com>
2023-09-02 18:13:55 +0300

    * styles cart

--- client/src/components/buttons/quantityBtns/QuantityBtns.jsx
+++ client/src/components/buttons/quantityBtns/QuantityBtns.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
 import Button from "../button";
 import "./quantityBtns.scss";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 function QuantityBtns({ handleIncrement, handleDecrement, count = 1, className }) {
 
 	return (
 		<div className={`quantityBtns ${className}`}>
-			<Button action={handleDecrement}>-</Button>
-			<span>{count}</span>
-			<Button action={handleIncrement}>+</Button>
+			<Button action={handleDecrement}><AiOutlineMinus /></Button>
+			<span className="font-bold">{count}</span>
+			<Button action={handleIncrement}><AiOutlinePlus /></Button>
 		</div>
 
 	);
