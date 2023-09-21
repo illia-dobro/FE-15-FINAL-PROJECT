c92153f706b70396748a59c2c63a534f58d7fca1
illia-dobro <illia.gopro@gmail.com>
2023-09-21 16:59:43 +0300

    Update catalogApi.js

--- client/src/app/services/catalogApi.js
+++ client/src/app/services/catalogApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const catalogApi = createApi({
 	reducerPath: "catalogApi",
 
-	baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/api/catalog" }),
+	baseQuery: fetchBaseQuery({ baseUrl: "https://f-15.onrender.com/api/catalog" }),
 
 	endpoints: (builder) => ({
 		getCategories: builder.query({
