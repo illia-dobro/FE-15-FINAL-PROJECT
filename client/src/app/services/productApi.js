1b2ac80e44b072693ef9a238bf1b1a7942a599f6
illia-dobro <illia.gopro@gmail.com>
2023-09-21 17:00:03 +0300

    Update productApi.js

--- client/src/app/services/productApi.js
+++ client/src/app/services/productApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const productApi = createApi({
   reducerPath: "productApi",
 
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/api/products" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://f-15.onrender.com/api/products" }),
 
   endpoints: (builder) => ({
     getAllProducts: builder.query({
